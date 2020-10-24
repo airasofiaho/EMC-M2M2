927f9e47a41ede6c0c50de3eec34916fd0889d46
airasofiaho <hoairait1a@gmail.com>
2020-10-24 08:54:26 +0800

    Update Average.cpp

--- Average.cpp
+++ Average.cpp
@@ -9,19 +9,18 @@ int main(){
 	
 	while(N > 20 || N<=0){
 		cout << "Error! number should in range of (1 to 20)." << endl;
-        cout << "Enter the number again: ";
-        cin >> N;
+        	cout << "Enter the number again: ";
+        	cin >> N;
 	}
 
-	for(count = 0; count < N; ++count)
-    {
-        cout << count + 1 << ". Enter number: ";
-        cin >> num[count];
-        sum += num[count];
-    }
+	for(count = 0; count < N; ++count){
+        	cout << count + 1 << ". Enter number: ";
+        	cin >> num[count];
+        	sum += num[count];
+    	}
 
-    average = sum / N;
-    cout << "Average = " << average << endl;
+    	average = sum / N;
+    	cout << "Average = " << average << endl;
 
 	system("PAUSE");
 }
